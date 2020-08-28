fca8a262620f103a68e66d348dd7497ff2871460
Moksha Shah <moksha1208@gmail.com>
2020-08-28 21:29:38 +0530

    jquery

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,39 +1,42 @@
-let data;
-const category = new Map();
-const subCategory = new Map();
-function getData(json) {
-    console.log(json);
-    data = json.feed.entry;
-    console.log(data);
-    populateDS(data);
-}
-const populateDS = data => {
-    console.log(data);
-    data.forEach(el => {
-        if (el.gs$cell.row != 1) {
-            if (el.gs$cell.col == 1) 
-                category.set(el.gs$cell.$t, el.gs$cell.row - 1);
-            else 
-                subCategory.set(el.gs$cell.row - 1, el.gs$cell.$t);
-        }
-    });
-    console.log(category);
-    console.log(subCategory);
-    populateButtons(category);
-}
-const populateButtons = category => {
-    category.forEach((value, key) => {
-        const markup1 = `<li class="filter-active">${key}</li>`;
-        const markup2 = `<li>${key}</li>`;
-        let button = document.getElementById('menu-flters');
-        button.insertAdjacentHTML('beforeend', (value == 1)? markup1: markup2);
-    })
-}
+!(function($) {
+    "use strict";
+    let data;
+    const category = new Map();
+    const subCategory = new Map();
+    function getData(json) {
+        console.log(json);
+        data = json.feed.entry;
+        console.log(data);
+        populateDS(data);
+    }
+    const populateDS = data => {
+        console.log(data);
+        data.forEach(el => {
+            if (el.gs$cell.row != 1) {
+                if (el.gs$cell.col == 1) 
+                    category.set(el.gs$cell.$t, el.gs$cell.row - 1);
+                else 
+                    subCategory.set(el.gs$cell.row - 1, el.gs$cell.$t);
+            }
+        });
+        console.log(category);
+        console.log(subCategory);
+        populateButtons(category);
+    }
+    const populateButtons = category => {
+        category.forEach((value, key) => {
+            const markup1 = `<li class="filter-active">${key}</li>`;
+            const markup2 = `<li>${key}</li>`;
+            let button = document.getElementById('menu-flters');
+            button.insertAdjacentHTML('beforeend', (value == 1)? markup1: markup2);
+        })
+    }
 
-$(window).on('load', function() {
-    $('#menu-flters li').on('click', function() {
-        $("#menu-flters li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        console.log($(this).data());
+    $(window).on('load', function() {
+        $('#menu-flters li').on('click', function() {
+            $("#menu-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
+            console.log($(this).data());
+        });
     });
-  });
+}
