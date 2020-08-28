f499a46224995ce454a5aa685b75955f6651e5e3
Moksha Shah <moksha1208@gmail.com>
2020-08-28 21:33:05 +0530

    aaa

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,21 +165,21 @@
   });
 
   // Menu list isotope and filter
-  $(window).on('load', function() {
-    var menuIsotope = $('.menu-container').isotope({
-      itemSelector: '.menu-item',
-      layoutMode: 'fitRows'
-    });
+  // $(window).on('load', function() {
+  //   var menuIsotope = $('.menu-container').isotope({
+  //     itemSelector: '.menu-item',
+  //     layoutMode: 'fitRows'
+  //   });
 
-    $('#menu-flters li').on('click', function() {
-      $("#menu-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
+  //   $('#menu-flters li').on('click', function() {
+  //     $("#menu-flters li").removeClass('filter-active');
+  //     $(this).addClass('filter-active');
 
-      menuIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-  });
+  //     menuIsotope.isotope({
+  //       filter: $(this).data('filter')
+  //     });
+  //   });
+  // });
 
   // Testimonials carousel (uses the Owl Carousel library)
   $(".events-carousel").owlCarousel({
