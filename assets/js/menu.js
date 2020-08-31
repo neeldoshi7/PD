606f7f8825ae082c04f7de4feaec564e59b8b872
Moksha Shah <moksha1208@gmail.com>
2020-08-31 10:19:46 +0530

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,32 @@ let data;
 const category = new Map();
 const subCategory = new Map();
 
+$(window).on('load', function() {
+    $('#menu-flters li').on('click', function() {
+        console.log(document.getElementById('menu-flters-sub'));
+        $("#menu-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+        $("#menu-flters-sub").empty();
+        let name = $(this).html();
+        let val = category.get(name);
+        if (subCategory.has(val)) {
+            let sub = subCategory.get(val);
+            let subarr = sub.split(",");
+            populateSubCatButtons(subarr);
+        } else {
+
+        }
+    });
+});
+
+$(document).on('click', '#menu-flters-sub li', function() {
+    console.log("clicked");
+    $("#menu-flters-sub li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+    let name = $(this).html();
+    console.log(name);
+});
+
 function getData(json) {
     data = json.feed.entry;
     populateDS(data);
@@ -44,29 +70,3 @@ const populateSubCatButtons = (sub) => {
         cat.insertAdjacentHTML('beforeend', (value == 0)? markup1: markup2);
     })
 }
-
-$(window).on('load', function() {
-    $('#menu-flters li').on('click', function() {
-        console.log(document.getElementById('menu-flters-sub'));
-        $("#menu-flters li").removeClass('filter-active');
-        $(this).addClass('filter-active');
-        $("#menu-flters-sub").empty();
-        let name = $(this).html();
-        let val = category.get(name);
-        if (subCategory.has(val)) {
-            let sub = subCategory.get(val);
-            let subarr = sub.split(",");
-            populateSubCatButtons(subarr);
-        } else {
-
-        }
-    });
-});
-
-$(document).on('click', '#menu-flters-sub li', function() {
-    console.log("clicked");
-    $("#menu-flters-sub li").removeClass('filter-active');
-    $(this).addClass('filter-active');
-    let name = $(this).html();
-    console.log(name);
-});
