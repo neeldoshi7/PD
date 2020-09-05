c9ca72e22d6d00073197e497f1da8fe79d81973f
Moksha Shah <moksha1208@gmail.com>
2020-09-05 12:26:17 +0530

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -136,7 +136,7 @@ const displayData = c => {
     let menulist = document.getElementById('menu-list');
     if (c === 2) {
         for (let i = 0; i < title.length; i++) {
-            const markup1 = `<div class="col-lg-6 menu-item filter-starters">
+            const markup1 = `<div class="col-lg-6 menu-item">
                                 <div class="menu-content">
                                     <a href="#">${title[i]}</a><span>${price[i]}</span>
                                 </div>
@@ -145,7 +145,7 @@ const displayData = c => {
         }
     } else if (c === 4) {
         for (let i = 0; i < title.length; i++) {
-            const markup2 = `<div class="col-lg-6 menu-item filter-starters">
+            const markup2 = `<div class="col-lg-6 menu-item">
                                 <div class="menu-content">
                                     <span>${code[i]}</span><a href="#">${title[i]}</a><span>${price[i]}</span>
                                 </div>
