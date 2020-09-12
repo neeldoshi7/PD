0ab19bc1b20b4b0d7f8835f0dcc687e81fe98163
Moksha Shah <moksha1208@gmail.com>
2020-09-12 11:06:50 +0530

    hide

---
+++ assets/js/order.js
@@ -0,0 +1,50 @@
+const getPages = json => {
+    pages = json.feed.entry;
+    populatePages(pages);
+}
+
+const populatePages = pages => {
+    for (let i = 0; i < pages.length; i += 2) {
+        pageNo.set(pages[i].gs$cell.$t, pages[i + 1].gs$cell.$t);
+    }
+}
+
+const getData = json => {
+    data = json.feed.entry;
+    populateDS(data);
+}
+
+const populateDS = data => {
+    data.forEach(el => {
+        if (el.gs$cell.row != 1) {
+            if (el.gs$cell.col == 1) 
+                category.set(el.gs$cell.$t, el.gs$cell.row - 1);
+            else 
+                subCategory.set(el.gs$cell.row - 1, el.gs$cell.$t);
+        }
+    });
+    populateCatDropdown(category);
+    // $("#menu-flters li.filter-active").click();
+}
+
+const populateCatDropdown = category => {
+    category.forEach((value, key) => {
+        const markup1 = `<a class="dropdown-item" href="#item1" selected>${key}</a>`;
+        const markup2 = `<a class="dropdown-item" href="#item1" >${key}</a>`;
+        let cat = document.querySelector('.cat');
+        cat.insertAdjacentHTML('beforeend', (value == 1)? markup1: markup2);
+    })
+}
+
+const populateSubCatButtons = sub => {
+    sub.forEach((el, value) => {
+        const markup1 = `<li class="filter-active">${el}</li>`;
+        const markup2 = `<li>${el}</li>`;
+        let cat = document.getElementById('menu-flters-sub');
+        cat.insertAdjacentHTML('beforeend', (value == 0)? markup1: markup2);
+    })
+}
+
+const getPageNo = name => {
+    return pageNo.get(name);
+}
