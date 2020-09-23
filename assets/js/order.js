99733d644046b28135246c3c34d1c1241d852090
Moksha Shah <moksha1208@gmail.com>
2020-09-23 10:38:24 +0530

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -177,6 +177,7 @@ $(document).on('click', '.icofont-plus-circle', function() {
 });
 
 changeTotal = () => {
+    total.toFixed(2);
     console.log(total);
     $('.modal-totalcost').text(`$${total}`);
 }
