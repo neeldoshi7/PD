5e4c1a65aedb845e3a560802cbe3afb292c86193
Moksha Shah <moksha1208@gmail.com>
2020-09-24 12:08:54 +0530

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -183,7 +183,7 @@ $(document).on('click', '.form-check', function() {
     console.log("clicked");
     if ($('.form-check-input').is(':checked')) {
         console.log("checked");
-        console.log($(this).children('.modal-extraitemprice'));
+        console.log($(this).children('.modal-extraitemprice').text());
         // let extracost = parseFloat($(this).children().innerHTML());
         // console.log(extracost);
         // console.log(typeof extracost);
