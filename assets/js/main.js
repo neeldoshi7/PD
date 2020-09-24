7ce2f5410c96e680cf66ec1fbde433a64b7cdc44
Moksha Shah <moksha1208@gmail.com>
2020-09-24 15:51:36 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -151,6 +151,7 @@
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
+      console.log($('.back-to-top'));
       $('.back-to-top').fadeIn('slow');
     } else {
       $('.back-to-top').fadeOut('slow');
