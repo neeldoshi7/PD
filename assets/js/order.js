7e3925f9487d8aefbe1c410b9922eddc90a2da5b
Moksha Shah <moksha1208@gmail.com>
2020-09-24 15:48:36 +0530

    Update order.js

--- assets/js/order.js
+++ assets/js/order.js
@@ -204,9 +204,9 @@ changeTotal = () => {
 
 //cart-button
 $(window).scroll(function() {
-    console.log('scrolled');
     if ($(this).scrollTop() > 100) {
         console.log('scrolled more');
+        console.log($('.cart-button'));
       $('.cart-button').fadeIn('slow');
     } else {
       $('.cart-button').fadeOut('slow');
