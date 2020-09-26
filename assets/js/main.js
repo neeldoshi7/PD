4912dc9b451b72a19235aea4e34e4085975de1d5
Moksha Shah <moksha1208@gmail.com>
2020-09-26 09:52:08 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,8 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-!(function($) {
-  "use strict";
+// !(function($) {
+//   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
@@ -204,4 +204,4 @@
     $('.venobox').venobox();
   });
 
-})(jQuery);
+// })(jQuery);
