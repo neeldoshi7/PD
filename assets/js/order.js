e89bde84354ab4cca5e8921a61038e0f13d1835f
Moksha Shah <moksha1208@gmail.com>
2020-10-10 18:01:22 +0530

    q

--- assets/js/order.js
+++ assets/js/order.js
@@ -268,3 +268,16 @@ $(document).on('click', '.modal-addtocart', function() {
     console.log(state.cart.items);
     $('.cart-item-number').text(state.cart.items.length);
 })
+
+$(document).ready(function(){
+    $('.count').prop('disabled', true);
+    $(document).on('click','.plus',function(){
+        $('.count').val(parseInt($('.count').val()) + 1 );
+    });
+    $(document).on('click','.minus',function(){
+        $('.count').val(parseInt($('.count').val()) - 1 );
+        if ($('.count').val() == 0) {
+            $('.count').val(1);
+        }
+    });
+});
