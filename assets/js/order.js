c25d5452c25a805602bcea512dc09881781a5fbb
Moksha Shah <moksha1208@gmail.com>
2020-10-11 11:17:59 +0530

    mm

--- assets/js/order.js
+++ assets/js/order.js
@@ -269,15 +269,3 @@ $(document).on('click', '.modal-addtocart', function() {
     $('.cart-item-number').text(state.cart.items.length);
 })
 
-$(document).ready(function(){
-    $('.count').prop('disabled', true);
-    $(document).on('click','.plus',function(){
-        $('.count').val(parseInt($('.count').val()) + 1 );
-    });
-    $(document).on('click','.minus',function(){
-        $('.count').val(parseInt($('.count').val()) - 1 );
-        if ($('.count').val() == 0) {
-            $('.count').val(1);
-        }
-    });
-});
