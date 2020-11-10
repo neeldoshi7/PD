c177c5d15f25b896422192e89f633680c0a9520a
Moksha Shah <moksha1208@gmail.com>
2020-11-10 20:11:49 +0530

    Update getAuth.php

--- assets/php/getAuth.php
+++ assets/php/getAuth.php
@@ -2,6 +2,23 @@
 
 include 'conn.php';
 
-$username = 
+$username = "neelpatel";
+$password = "12345678";
+
+$query = "SELECT password WHERE username like $username";
+
+$result = $conn->query($mysql_qry);
+
+if($result->num_rows === 1) {
+    while($row = $result->fetch_assoc()) {
+        if ($row["password"] === $password) {
+            print("Login");
+        }
+	}
+} else {
+    print("Unsuccessful");
+}
+
+$conn->close();
 
 ?>
