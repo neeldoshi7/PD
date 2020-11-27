6548dde38aff4520102e18c196cdf963f4952293
Moksha Shah <moksha1208@gmail.com>
2020-11-27 19:35:08 +0530

    o

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -173,7 +173,6 @@ const displayData = c => {
             menulist.insertAdjacentHTML('beforeend', markup2);
         }
     } else if (c === 6) {
-        // let pizzamsg = document.getElementById   ('pizza-msg');
         const markup = `<div>Only available in New Brunswick</div>`;
         menulist.insertAdjacentHTML('beforebegin', markup);
         for (let i = 0; i < title.length; i++) {
