0f5f9755f3142d9b0cd3067831da0a27298b3a8e
Moksha Shah <moksha1208@gmail.com>
2021-01-03 18:08:04 +0530

    alert

--- assets/php/conn.php
+++ assets/php/conn.php
@@ -14,5 +14,5 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 ?>
--- assets/php/getAuth.php
+++ assets/php/getAuth.php
@@ -12,20 +12,24 @@ $result = mysqli_query($conn, $query);
 if(mysqli_num_rows($result) === 1) {
     while($row = $result->fetch_assoc()) {
         if ($row["password"] === $password) {
-            print("Login");
-            header('Location: .\..\..\adminpage.html');
+            // print("Login");
+            echo "<script>alert('Logged in');";
+            echo "window.location.pathname = '/PD/adminpage.html'</script>";
+            // header('Location: .\..\..\adminpage.html');
             exit;
         } else {
-            print("Wrong pass");
-            echo "<script>alert('Wrong Credentials');</script>";
+            // print("Wrong pass");
+            echo "<script>alert('Wrong Credentials');";
+            echo "window.location.pathname = '/PD/admin.html'</script>";
             // echo "<script>document.location = 'admin.html';</script>";
-            header('Location: .\..\..\admin.html');
+            // header('Location: .\..\..\admin.html');
         }
 	}
 } else {
-    print("Unsuccessful");
-    // echo "<script>alert('Wrong Credentials');</script>";
-    header('Location: .\..\..\admin.html');
+    // print("Unsuccessful");
+    echo "<script>alert('Wrong Credentials');";
+    echo "window.location.pathname = '/PD/admin.html'</script>";
+    // header('Location: .\..\..\admin.html');
 }
 
 $conn->close();
