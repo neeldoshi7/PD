e4f6543cb5f5ce7b9cf55e2b2e6b1857f8657943
Moksha Shah <moksha1208@gmail.com>
2021-01-03 17:41:03 +0530

    Update getpdf.php

--- assets/php/getpdf.php
+++ assets/php/getpdf.php
@@ -2,34 +2,49 @@
 
 include 'conn.php';
 
-// Query for a list of all existing files
-$sql = 'SELECT `pdf_file` FROM `pdf`';
-$result = $conn->query($sql);
- 
-// Check if it was successfull
-if($result) {
-    // Make sure there are some files in there
-    if($result->num_rows == 1) {
+$query = 'SELECT `todays_special` FROM `specials`';
+$bool = $conn->query($query);
 
-        $row = mysqli_fetch_assoc($result);
+if ($bool) {
+    if ($bool->num_rows == 1) {
+        $row = mysqli_fetch_assoc($bool);
+        if ($row['todays_specials'] === 1) {
+            // Query for a list of all existing files
+            $sql = 'SELECT `pdf_file` FROM `pdf`';
+            $result = $conn->query($sql);
+            
+            // Check if it was successfull
+            if($result) {
+                // Make sure there are some files in there
+                if($result->num_rows == 1) {
 
-        header("Content-type:application/pdf");
+                    $row = mysqli_fetch_assoc($result);
 
-        echo $row['pdf_file'];
- 
-        
-    }
-    else {
+                    header("Content-type:application/pdf");
 
+                    echo $row['pdf_file'];
+            
+                    
+                } else {
+                    echo '<p>There are no files in the database</p>'; 
+                }
+            } else {
+                echo 'Error! SQL query failed:';
+                echo "<pre>{$conn->error}</pre>";
+            }
+        } else {
+            echo "<script>alert('Not Available');</script>";
+            header('Location: .\..\..\index.html');
+        }
+    } else {
         echo '<p>There are no files in the database</p>';
-        
     }
-}
-else
-{
+} else {
     echo 'Error! SQL query failed:';
-    echo "<pre>{$conn->error}</pre>";
+                echo "<pre>{$conn->error}</pre>";
 }
+
+
 
 $conn->close();
 
